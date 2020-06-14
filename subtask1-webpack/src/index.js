d23c6d3215fa89b7474060bf0671bb4eca83825c
dmitrii1600 <shevchenko.dmitrii2000@gmail.com>
2020-06-14 14:51:58 +0300

    Second commit asset setup

--- subtask1-webpack/src/index.js
+++ subtask1-webpack/src/index.js
@@ -1,10 +1,18 @@
 import join from "lodash/join"
+import './style.css'
+import Icon from './smile_icon.svg'
 
 //for example
 function testSubtask() {
     const elementDiv = document.createElement("div");
 
     elementDiv.innerText = join(['Webpack', 'Project', 'Hometask1'], ' ');
+    elementDiv.classList.add('title');
+
+    const smileIcon = new Image();
+    smileIcon.src = Icon;
+    smileIcon.classList.add('icon');
+    elementDiv.appendChild(smileIcon);
 
     return elementDiv
 
