76d8bb748afe4a9affdca635ad0b28cb4d64fd74
dmitrii1600 <shevchenko.dmitrii2000@gmail.com>
2020-06-14 15:29:50 +0300

    Third commit output config

--- subtask1-webpack/src/index.js
+++ subtask1-webpack/src/index.js
@@ -1,18 +1,25 @@
 import join from "lodash/join"
-import './style.css'
-import Icon from './smile_icon.svg'
+import './css/style.css'
+import Icon from './img/smile_icon.svg'
+import printMe from "./print"
 
 //for example
 function testSubtask() {
-    const elementDiv = document.createElement("div");
+    const elementDiv = document.createElement('div');
+    const btn = document.createElement('button');
 
-    elementDiv.innerText = join(['Webpack', 'Project', 'Hometask1'], ' ');
+    elementDiv.innerText = join(['Webpack', 'Project', 'Subtask1'], ' ');
     elementDiv.classList.add('title');
 
     const smileIcon = new Image();
     smileIcon.src = Icon;
     smileIcon.classList.add('icon');
     elementDiv.appendChild(smileIcon);
+
+    btn.innerHTML = 'Click me and check the console!';
+    btn.onclick = printMe;
+    btn.classList.add('btn');
+    elementDiv.appendChild(btn);
 
     return elementDiv
 
